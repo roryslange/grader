10286324f99e0657d5dc86d57fbf8f901ce732fc
Rory Lange <rorystrattonlange@gmail.com>
2025-11-23 14:12:49 -0500

    return path from download

--- grader/src/dataloader.py
+++ grader/src/dataloader.py
@@ -3,6 +3,7 @@ import kagglehub as kh
 def downloadClimbingHolds():
     path = kh.dataset_download("alirezachahardoli/object-detection-climbing-holds")
     print("Path to dataset files:", path)
+    return path
 
 def downloadClimbingHoldsLocally():
     pass
