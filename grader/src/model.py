62c37710f29fa9055afbba3a74d1480089fe9bc8
Rory Lange <rorystrattonlange@gmail.com>
2025-11-24 23:38:18 -0500

    train the model

--- grader/src/model.py
+++ grader/src/model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
-from .dataloader import downloadClimbingHolds
 
 def test():
-    model = YOLO(model="yolo11n.pt")
-    datapath = downloadClimbingHolds()
+    model = YOLO(model="yolov8n.pt")
+    model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device='cpu')
+    
     
