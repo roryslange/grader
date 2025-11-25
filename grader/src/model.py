07609c4b6e64d4ae870c2ba6c12868ee7af8e71b
Rory Lange <rorystrattonlange@gmail.com>
2025-11-24 23:56:12 -0500

    change method name

--- grader/src/model.py
+++ grader/src/model.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-def test():
+def train():
     model = YOLO(model="yolov8n.pt")
     model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device='cpu')
     
