35ef686583a44e2891c019e41b8acc3cb60334d8
Rory Lange <rorystrattonlange@gmail.com>
2025-11-25 23:31:50 -0500

    test on an image

--- grader/src/model.py
+++ grader/src/model.py
@@ -1,7 +1,13 @@
 from ultralytics import YOLO
+from PIL import ImageGrab
 
 def train():
-    model = YOLO(model="yolov8n.pt")
+    model = YOLO(model="yolov5s.pt")
     model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device='cpu')
     
+def test():
+    model = YOLO(model="yolov8n.pt")
+    image = "./grader/person.jpg"
+    results = model(image)
+    results[0].show()
     
