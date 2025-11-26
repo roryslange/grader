0996c3778857635b8061cd28ac26025d2eaf00b9
Rory Lange <rorystrattonlange@gmail.com>
2025-11-25 23:33:14 -0500

    add testing and models folder

--- grader/src/model.py
+++ grader/src/model.py
@@ -6,7 +6,7 @@ def train():
     model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device='cpu')
     
 def test():
-    model = YOLO(model="yolov8n.pt")
+    model = YOLO(model="./grader/models/yolov8n.pt")
     image = "./grader/person.jpg"
     results = model(image)
     results[0].show()
