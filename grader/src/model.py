b146e7e2081924b5c14c62dabbcb2bf94e99977c
Rory Lange <rorystrattonlange@gmail.com>
2025-12-02 00:44:48 -0500

    edit train script

--- grader/src/model.py
+++ grader/src/model.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 def train():
-    model = YOLO(model="yolov5s.pt")
-    model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device='cpu')
+    model = YOLO(model="./grader/models/yolo11s.pt")
+    model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device=0)
     
 def test():
     model = YOLO(model="./grader/models/yolov8n.pt")
