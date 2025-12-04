44c7a37c91403ed91e4ca9cc17d36344f7e4b0a2
Rory Lange <rorystrattonlange@gmail.com>
2025-12-04 00:18:02 -0500

    add docstring

--- grader/src/model.py
+++ grader/src/model.py
@@ -1,6 +1,14 @@
 from ultralytics import YOLO
 import os
 
+"""
+    tools for training yolo models
+
+    models to use:
+    yolov8n-seg.pt (YOLO V8 segmentation fork)
+    yolov11n.pt (requires labels to be converted to bounding boxes)
+"""
+
 def train():
     model = YOLO(model="./grader/models/yolo11s.pt")
     model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device=0)
@@ -13,7 +21,7 @@ def test():
     
 def testOnHolds():
     cwd = os.getcwd()
-    model = YOLO(model="yolo11s.pt")
+    model = YOLO(model="yolov8n-seg.pt")
     images_path = os.path.join(cwd, "grader/datasets/images/valid_images")
     for i in range(3):
         results = model(os.path.join(images_path, os.listdir(images_path)[i]))
