9374ac6bdad1891ed85e6e5b0d0c7ccaeaded783
Rory Lange <rorystrattonlange@gmail.com>
2025-12-06 15:00:17 -0500

    add constants for training stuff

--- grader/src/model.py
+++ grader/src/model.py
@@ -9,9 +9,18 @@ import os
     yolov11n.pt (requires labels to be converted to bounding boxes)
 """
 
+MODEL_PATH_YOLOV8N_SEG = os.curdir.join(["grader/models/yolov8n-seg.pt"])
+DATASET_YAML_PATH = "./grader/src/dataset.yaml"
+EPOCHS = 50
+IMG_SIZE = 640
+BATCH_SIZE = 16
+DEVICE = 0 # tbd refactor to use cpu if no gpu available
+BEST_WEIGHTS_PATH = os.curdir.join(["runs/segment/train/weights/best.pt"])
+LAST_WEIGHTS_PATH = os.curdir.join(["runs/segment/train/weights/last.pt"])
+
 def train():
-    model = YOLO(model="./grader/models/yolo11s.pt")
-    model.train(data='./grader/src/dataset.yaml', epochs=50, imgsz=640, batch=16, device=0)
+    model = YOLO(model=MODEL_PATH_YOLOV8N_SEG)
+    model.train(data=DATASET_YAML_PATH, epochs=EPOCHS, imgsz=IMG_SIZE, batch=BATCH_SIZE, device=DEVICE)
     
 def test():
     model = YOLO(model="./grader/models/yolov8n.pt")
@@ -21,7 +30,7 @@ def test():
     
 def testOnHolds():
     cwd = os.getcwd()
-    model = YOLO(model="yolov8n-seg.pt")
+    model = YOLO(model=BEST_WEIGHTS_PATH)
     images_path = os.path.join(cwd, "grader/datasets/images/valid_images")
     for i in range(3):
         results = model(os.path.join(images_path, os.listdir(images_path)[i]))
