fd31d2d021c21ce29eb459f52c0355940238993a
Rory Lange <rorystrattonlange@gmail.com>
2025-12-09 23:35:19 -0500

    I fixed the model

--- grader/src/model.py
+++ grader/src/model.py
@@ -9,14 +9,14 @@ import os
     yolov11n.pt (requires labels to be converted to bounding boxes)
 """
 
-MODEL_PATH_YOLOV8N_SEG = os.curdir.join(["grader/models/yolov8n-seg.pt"])
+MODEL_PATH_YOLOV8N_SEG = os.curdir.join(["grader/models/yolov8n.pt"])
 DATASET_YAML_PATH = "./grader/src/dataset.yaml"
 EPOCHS = 50
 IMG_SIZE = 640
 BATCH_SIZE = 16
 DEVICE = 0 # tbd refactor to use cpu if no gpu available
-BEST_WEIGHTS_PATH = os.curdir.join(["runs/segment/train/weights/best.pt"])
-LAST_WEIGHTS_PATH = os.curdir.join(["runs/segment/train/weights/last.pt"])
+BEST_WEIGHTS_PATH = os.curdir.join(["runs/runs/detect/train5/weights/best.pt"])
+LAST_WEIGHTS_PATH = os.curdir.join(["runs/runs/detect/train5/weights/last.pt"])
 
 def train():
     model = YOLO(model=MODEL_PATH_YOLOV8N_SEG)
